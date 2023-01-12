3e4cad30e78145821d06c70e1c6b25ab8438e668
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-01-12 13:10:07 +0100

    setup boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "../components/Home/Home";
+import Section from "../components/Section/Section"
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Section/>
+    </div>
+  );
+}
+
+export default App;
